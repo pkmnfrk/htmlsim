7563da82ede45ede33971f91210af5fd0594e800
JJ Mifsud <jmifsud@klick.com>
2014-11-21 12:54:35 -0500

    I forget if this commit is useful

--- Veeva Simulator/ViewController.m
+++ Veeva Simulator/ViewController.m
@@ -21,32 +21,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    NSString *fileToLoad = [self findHtmlIndex];
     
-    NSError *error = nil;
-    NSString *yourFolderPath = @"Documents";
-    NSArray  *yourFolderContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:yourFolderPath error:&error];
-    NSString *fileToLoad = nil;
+//    NSError *error = nil;
+//    NSString *yourFolderPath = @"Documents";
+//    NSArray  *yourFolderContents = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:yourFolderPath error:&error];
+//    NSString *fileToLoad = nil;
     
-    for (NSString *object in yourFolderContents) {
-//        NSLog(@"%@", object);
-//        NSLog(@"%@", [object class]);
-//        NSLog(@"%@", [object pathExtension]);
-        
-        if ([[object pathExtension] isEqualToString:@"html"])
-        {
-            fileToLoad = [object substringToIndex: [object length] - 5];
-            
-            
-            NSLog(@"%@", fileToLoad);
-            break;
-        }
-        
-        else {
-            NSLog(@"NO");
-        }
-        
-        NSLog(@"===================================");
-    }
+//    for (NSString *object in yourFolderContents) {
+//        if ([[object pathExtension] isEqualToString:@"html"])
+//        {
+//            fileToLoad = [object substringToIndex: [object length] - 5];
+//            
+//            
+//            NSLog(@"%@", fileToLoad);
+//            break;
+//        }
+//        
+//        else {
+//            NSLog(@"NO");
+//        }
+//        
+//        NSLog(@"===================================");
+//    }
     
     if (!fileToLoad) {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Sup Bro" message:@"Yo got no HTMLz in here" delegate:self cancelButtonTitle:@"Ok, Cool" otherButtonTitles:nil];
@@ -54,9 +52,10 @@
         return;
     }
     
-    fileToLoad = [NSString stringWithFormat:@"html/%@",  fileToLoad];
+    NSURL * resourcePathURL = [NSURL URLWithString:fileToLoad];
     
-    NSURL * resourcePathURL = [NSURL fileURLWithPath: [[NSBundle mainBundle] pathForResource:@"html/index" ofType:@"html"]];
+    NSLog(@"%@", resourcePathURL);
+    
     if(resourcePathURL)
     {
         NSURLRequest * req = [NSURLRequest requestWithURL: resourcePathURL];
@@ -64,6 +63,52 @@
     }
 
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (NSString *) findHtmlIndex {
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    NSString *documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+
+    NSURL *directoryURL = [NSURL URLWithString:documentsPath];
+    NSArray *keys = [NSArray arrayWithObject:NSURLIsDirectoryKey];
+    
+    NSDirectoryEnumerator *enumerator = [fileManager
+                                         enumeratorAtURL:directoryURL
+                                         includingPropertiesForKeys:keys
+                                         options:0
+                                         errorHandler:^(NSURL *url, NSError *error) {
+                                             // Handle the error.
+                                             // Return YES if the enumeration should continue after the error.
+                                             return YES;
+                                         }];
+    
+    for (NSURL *url in enumerator) {
+        NSError *error;
+        NSNumber *isDirectory = nil;
+        
+        if (! [url getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:&error]) {
+            // handle error
+        }
+        else if (! [isDirectory boolValue]) {
+            // No error and it’s not a directory; do something with the file
+            
+            NSString *urlString = [url absoluteString];
+            
+  
+            
+            if ([[url pathExtension] isEqualToString:@"html"]) {
+//                NSLog(urlString);
+                return [urlString substringToIndex:[urlString length] -5];
+            }
+            
+
+        }
+        
+        NSLog(@"============");
+    }
+    
+    return nil;
+//    return fileToOpen;
 }
 
 - (void)didReceiveMemoryWarning {
